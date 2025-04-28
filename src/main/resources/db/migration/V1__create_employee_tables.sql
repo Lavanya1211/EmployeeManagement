9fef0b349779ea41e729743847c3198030c47fbe
Lavanya1211 <lavanya00baskar@gmail.com>
2025-04-28 22:59:49 +0530

    initial table ddl

--- src/main/resources/db/migration/V1__create_employee_tables.sql
+++ src/main/resources/db/migration/V1__create_employee_tables.sql
@@ -1,76 +1,88 @@
--- DEPARTMENT Table
 CREATE TABLE department (
-    department_id SERIAL PRIMARY KEY,
-    department_name VARCHAR(100) NOT NULL,
-    description VARCHAR(100),
-    status VARCHAR(20) CHECK (status IN ('active', 'inactive')),
-    created_by VARCHAR(100),
-    updated_by VARCHAR(100),
-    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    department_id NUMBER PRIMARY KEY,
+    department_name VARCHAR2(100) NOT NULL,
+    description VARCHAR2(100),
+    status VARCHAR2(20) DEFAULT 'active' CHECK (status IN ('active', 'inactive')),
+    created_by VARCHAR2(100),
+    updated_by VARCHAR2(100),
+    created_on TIMESTAMP DEFAULT SYSTIMESTAMP,
+    updated_on TIMESTAMP DEFAULT SYSTIMESTAMP
 );
 
--- JOB_TITLE Table
+CREATE SEQUENCE department_seq START WITH 1 INCREMENT BY 1;
+
+
 CREATE TABLE job_title (
-    job_title_id SERIAL PRIMARY KEY,
-    job_title VARCHAR(100) NOT NULL,
-    description VARCHAR(100),
-    status VARCHAR(20) CHECK (status IN ('active', 'inactive')),
-    created_by VARCHAR(100),
-    updated_by VARCHAR(100),
-    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    job_title_id NUMBER PRIMARY KEY,
+    job_title VARCHAR2(100) NOT NULL,
+    description VARCHAR2(100),
+    status VARCHAR2(20) DEFAULT 'active' CHECK (status IN ('active', 'inactive')),
+    created_by VARCHAR2(100),
+    updated_by VARCHAR2(100),
+    created_on TIMESTAMP DEFAULT SYSTIMESTAMP,
+    updated_on TIMESTAMP DEFAULT SYSTIMESTAMP
 );
+
+CREATE SEQUENCE job_title_seq START WITH 1 INCREMENT BY 1;
+
+
 
 CREATE TABLE employee (
-    employee_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    phone_number VARCHAR(20),
+    employee_id NUMBER PRIMARY KEY,
+    first_name VARCHAR2(100) NOT NULL,
+    last_name VARCHAR2(100) NOT NULL,
+    email VARCHAR2(150) UNIQUE NOT NULL,
+    phone_number VARCHAR2(20),
     joining_date DATE NOT NULL,
-    department_id INT REFERENCES department(department_id),
-    job_title_id INT REFERENCES job_title(job_title_id),
-    manager_id INT,
+    department_id NUMBER REFERENCES department(department_id) ON DELETE SET NULL,
+    job_title_id NUMBER REFERENCES job_title(job_title_id) ON DELETE SET NULL,
+    manager_id NUMBER REFERENCES employee(employee_id) ON DELETE SET NULL,
     date_of_birth DATE NOT NULL,
-    created_by VARCHAR(100),
-    updated_by VARCHAR(100),
-    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(20) CHECK (status IN ('active', 'inactive', 'abscond'))
+    created_by VARCHAR2(100),
+    updated_by VARCHAR2(100),
+    created_on TIMESTAMP DEFAULT SYSTIMESTAMP,
+    updated_on TIMESTAMP DEFAULT SYSTIMESTAMP,
+    status VARCHAR2(20) DEFAULT 'active' CHECK (status IN ('active', 'inactive', 'abscond'))
 );
 
--- EMPLOYEE_ADDITIONAL_INFORMATION Table
+CREATE SEQUENCE employee_seq START WITH 1 INCREMENT BY 1;
+
+
+
 CREATE TABLE employee_additional_information (
-    emp_basic_info_id SERIAL PRIMARY KEY,
-    employee_id INT REFERENCES employee(employee_id),
-    gender VARCHAR(20) CHECK (gender IN ('Female', 'Male', 'Others')),
-    emp_email VARCHAR(150),
-    linkedin_profile_url VARCHAR(255),
-    emergency_contact VARCHAR(100),
-    marital_status VARCHAR(50),
-    address_line_1 VARCHAR(255),
-    address_line_2 VARCHAR(255),
-    city VARCHAR(100),
-    state VARCHAR(100),
-    country VARCHAR(100),
-    zip_code VARCHAR(20),
-    created_by VARCHAR(100),
-    updated_by VARCHAR(100),
-    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    emp_basic_info_id NUMBER PRIMARY KEY,
+    employee_id NUMBER UNIQUE REFERENCES employee(employee_id) ON DELETE CASCADE,
+    gender VARCHAR2(20) CHECK (gender IN ('Female', 'Male', 'Others')),
+    emp_email VARCHAR2(150),
+    linkedin_profile_url VARCHAR2(255),
+    emergency_contact VARCHAR2(100),
+    marital_status VARCHAR2(50),
+    address_line_1 VARCHAR2(255),
+    address_line_2 VARCHAR2(255),
+    city VARCHAR2(100),
+    state VARCHAR2(100),
+    country VARCHAR2(100),
+    zip_code VARCHAR2(20),
+    created_by VARCHAR2(100),
+    updated_by VARCHAR2(100),
+    created_on TIMESTAMP DEFAULT SYSTIMESTAMP,
+    updated_on TIMESTAMP DEFAULT SYSTIMESTAMP
 );
 
+CREATE SEQUENCE emp_basic_info_id_seq START WITH 1 INCREMENT BY 1;
 
 
--- SALARY Table
+
 CREATE TABLE salary (
-    salary_id SERIAL PRIMARY KEY,
-    employee_id INT REFERENCES employee(employee_id),
-    description  VARCHAR(100),
-    status VARCHAR(20) CHECK (status IN ('active', 'inactive')),
-    created_by VARCHAR(100),
-    updated_by VARCHAR(100),
-    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    salary_id NUMBER PRIMARY KEY,
+    employee_id NUMBER REFERENCES employee(employee_id) ON DELETE CASCADE,
+    salary_amount NUMBER(10,2) NOT NULL,
+    description VARCHAR2(100),
+    status VARCHAR2(20) DEFAULT 'active' CHECK (status IN ('active', 'inactive')),
+    created_by VARCHAR2(100),
+    updated_by VARCHAR2(100),
+    created_on TIMESTAMP DEFAULT SYSTIMESTAMP,
+    updated_on TIMESTAMP DEFAULT SYSTIMESTAMP
 );
+
+CREATE SEQUENCE salary_id_seq START WITH 1 INCREMENT BY 1;
